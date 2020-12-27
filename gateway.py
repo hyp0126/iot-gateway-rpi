2170393e8be290adfb8ec9f91eaef8f53f3033fa
yhong <hyp0126@gmail.com>
2020-12-27 01:22:00 -0500

    Uno HC-06 BT -> /dev/rfcomm0 (rpi) -> MQTT (rpi)

---
+++ gateway.py
@@ -0,0 +1,26 @@
+#rpi: python3
+#HC-06: Paring password: 1234
+#sudo apt-get install python-serial
+#sudo pip3 install paho-mqtt
+#Connect BT: sudo rfcomm connect hci0 98:D3:32:20:6C:CE
+
+import serial
+from time import sleep
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+
+Broker = "192.168.2.62"
+
+bluetoothSerial = serial.Serial( "/dev/rfcomm0", baudrate=9600 )
+ 
+client = mqtt.Client()
+client.connect(Broker, 1883, 60)
+client.loop_start()
+
+while True:
+    input = str(bluetoothSerial.readline())
+    print(input)
+    inputs = input.split('/')
+    topic = '/'.join(inputs[0:len(inputs)-1])
+    client.publish(topic, inputs[len(inputs)-1])
+    print(inputs)
