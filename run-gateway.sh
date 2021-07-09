217444fe13eb99c69c9489c09cf38ea7332a40e6
yhong <hyp0126@gmail.com>
2021-07-08 17:29:49 -0900

    fix shell script

--- run-gateway.sh
+++ run-gateway.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo rfcomm connect hci0 98:D3:32:20:6C:CE &
-python3 /home/pi/iot-gateway-rpi/gateway.py
+python3 /home/pi/iot-gateway-rpi/gateway.py &
 python3 /home/pi/iot-gateway-rpi/web_streaming_server.py
